7eb3c78842e170eb3cf5ad69a73c50e439e82712
sbechoe <s.bechoe@rocmondriaan.nl>
2022-10-21 16:39:44 +0200

    HTML/CSS wireframe build

--- js/main.js
+++ js/main.js
@@ -1,41 +1,48 @@
-const name = prompt("Wat is je naam?");
+// const startGame = confirm("Wil je het spel spelen?");
+// if (startGame == true) {
+//     playGame();
+// }
 let playerPoints = 20;
 let win;
 let lost;
 let draw;
 
-while (playerPoints != 0) {
-    let pcDice = Math.floor(Math.random() * 6) + 1;
-    let playerDice = Math.floor(Math.random() * 6) + 1;
-    alert("De computer gooit " + pcDice);
-    let guess = prompt("Hoger of lager?");
-    alert(name + " gooit " + playerDice);
+function playGame() {
+    const name = prompt("Wat is je naam?");
+    while (playerPoints != 0) {
+        let pcDice = Math.floor(Math.random() * 6) + 1;
+        let playerDice = Math.floor(Math.random() * 6) + 1;
+        alert("De computer gooit " + pcDice);
+        let guess = prompt("Hoger of lager?");
+        alert(name + " gooit " + playerDice);
 
-    if (guess == "hoger") {
-        if (playerDice > pcDice) {
-            win = alert(name + " wint!!");
-            playerPoints += 5;
-        } else if (playerDice < pcDice) {
-            lost = alert("Computer wint");
-            playerPoints -= 5;
-        } else if (playerDice == pcDice) {
-            draw = alert("Het is gelijkspel");
+        if (guess == "hoger") {
+            if (playerDice > pcDice) {
+                win = alert(name + " wint!!");
+                playerPoints += 5;
+            } else if (playerDice < pcDice) {
+                lost = alert("Computer wint");
+                playerPoints -= 5;
+            } else if (playerDice == pcDice) {
+                draw = alert("Het is gelijkspel");
+            }
         }
-    }
-    if (guess == "lager") {
+        if (guess == "lager") {
 
-        if (playerDice < pcDice) {
-            win = alert(name + " wint!!");
-            playerPoints += 5;
-        } else if (playerDice > pcDice) {
-            lost = alert("Computer wint");
-            playerPoints -= 5;
-        } else if (playerDice == pcDice) {
-            draw = alert("Het is gelijkspel");
+            if (playerDice < pcDice) {
+                win = alert(name + " wint!!");
+                playerPoints += 5;
+            } else if (playerDice > pcDice) {
+                lost = alert("Computer wint");
+                playerPoints -= 5;
+            } else if (playerDice == pcDice) {
+                draw = alert("Het is gelijkspel");
+            }
         }
-    }
 
-    alert("Punten: " + playerPoints);
+        alert("Punten: " + playerPoints);
+    }
+    if (playerPoints == 0) {
+        alert("U heeft " + playerPoints + " punten, je hebt verloren!");
+    }
 }
-
-alert("U heeft "+ playerPoints+" punten, je hebt verloren!");
