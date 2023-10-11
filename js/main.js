ebd241d7f43fb0c770e29379e481554f15d805df
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2023-10-11 12:26:56 +0200

    bug fix + added ascii dices

--- js/main.js
+++ js/main.js
@@ -10,82 +10,99 @@ const message = document.querySelector('.message');
 const computerCoins = document.querySelector('.computerCoins');
 const playerCoins = document.querySelector('.playerCoins');
 
-let playerChoice = "Done";
+let playerState = "Start";
+let computerState = "Start"
 let computerDice1;
 let computerDice2;
 let computerTotal;
 let playerDice1;
 let playerDice2;
 let playerTotal;
-let bank = 50;
+let bankCoins = 50;
 let myCoins = 50;
-computerCoins.textContent = "Coins: " + bank;
+const dices = ["&#9856;", "&#9857;", "&#9858;", "&#9859;", "&#9860;", "&#9861;"];
+
+computerCoins.textContent = "Coins: " + bankCoins;
 playerCoins.textContent = "Coins: " + myCoins;
 
 computerBtn.addEventListener('click', throwComputer);
 playerBtn.addEventListener('click', throwPlayer);
+
 higherBtn.addEventListener('click', function () {
-    playerChoice = 'Higher';
-    checkChoice();
+    if (computerState == "Done") {
+        playerState = 'Higher';
+    } else {
+        checkChoice();
+    }
 });
 lowerBtn.addEventListener('click', function () {
-    playerChoice = 'Lower';
-    checkChoice();
+    if (computerState == "Done") {
+        playerState = 'Lower';
+    } else {
+        checkChoice();
+    }
 });
 
 function throwComputer() {
-    playerDice1 = undefined;
-    playerDice2 = undefined;
-    computerDice1 = Math.floor(Math.random() * 6) + 1;
-    computerDice2 = Math.floor(Math.random() * 6) + 1;
-    computerNumber1.textContent = computerDice1;
-    computerNumber2.textContent = computerDice2;
-    computerTotal = computerDice1 + computerDice2;
-    message.textContent = "Computer heeft " + computerTotal + " gegooid.";
-    playerChoice = "Empty"
+    if (computerState == "Start") {
+        playerDice1 = 0;
+        playerDice2 = 0;
+        computerDice1 = Math.floor(Math.random() * 6);
+        computerDice2 = Math.floor(Math.random() * 6);
+        computerNumber1.innerHTML = dices[computerDice1];
+        computerNumber2.innerHTML = dices[computerDice2];
+        computerTotal = computerDice1 + 1 + computerDice2 + 1;
+        message.textContent = "Computer heeft " + computerTotal + " gegooid.";
+        playerState = "Empty";
+        computerState = "Done";
+    }
 }
 
 function throwPlayer() {
-    checkChoice();
-    if (playerChoice !== "Done" && playerChoice !== "Empty") {
-        playerDice1 = Math.floor(Math.random() * 6) + 1;
-        playerDice2 = Math.floor(Math.random() * 6) + 1;
+    if (playerState !== "Start" && playerState !== "Empty") {
+        playerDice1 = Math.floor(Math.random() * 6);
+        playerDice2 = Math.floor(Math.random() * 6);
         playerTotal = playerDice1 + playerDice2;
         checkChoice();
-        playerNumber1.textContent = playerDice1;
-        playerNumber2.textContent = playerDice2;
-        playerChoice = "Done";
+        playerNumber1.innerHTML = dices[playerDice1];
+        playerNumber2.innerHTML = dices[playerDice2];
+        playerState = "Start";
+        computerState = "Start";
+    } else {
+        checkChoice();
     }
 }
 
 function checkChoice() {
-    if (playerChoice === "Higher") {
-        higher();
-    }
-    if (playerChoice === "Lower") {
-        lower();
-    }
-    if (playerChoice === "Empty") {
-        message.textContent = "Kies eerst Hoger of Lager";
-    }
-    if (playerChoice === "Done") {
-        message.textContent = "Computer moet gooien";
+    switch (playerState) {
+        case "Empty":
+            message.textContent = "Kies eerst Hoger of Lager";
+            break;
+        case "Start":
+            message.textContent = "Computer moet gooien";
+            break;
+        case "Higher":
+            higher();
+            break;
+        case "Lower":
+            lower();
+            break;
     }
 }
 
 function higher() {
     if (playerTotal > computerTotal) {
         message.textContent = "Speler wint!!" + " Jij gooide " + playerTotal;
-        bank -= 5;
+        bankCoins -= 5;
         myCoins += 5;
     } else if (playerTotal < computerTotal) {
         message.textContent = "Computer wint." + " Jij gooide " + playerTotal;
-        bank += 5;
+        bankCoins += 5;
         myCoins -= 5;
     } else if (playerTotal === computerTotal) {
         message.textContent = "Het is gelijkspel." + " Jij gooide ook " + playerTotal;
     }
-    computerCoins.textContent = "Coins: " + bank;
+    computerCoins.textContent = "Coins: " + bankCoins;
     playerCoins.textContent = "Coins: " + myCoins;
     checkCoins();
 
@@ -94,16 +111,16 @@ function higher() {
 function lower() {
     if (playerTotal < computerTotal) {
         message.textContent = "Speler wint!!" + " Jij gooide " + playerTotal;
-        bank -= 5;
+        bankCoins -= 5;
         myCoins += 5;
     } else if (playerTotal > computerTotal) {
         message.textContent = "Computer wint." + " Jij gooide " + playerTotal;
-        bank += 5;
+        bankCoins += 5;
         myCoins -= 5;
     } else if (playerTotal == computerTotal) {
         message.textContent = "Het is gelijkspel." + " Jij gooide ook " + playerTotal;
     }
-    computerCoins.textContent = "Coins: " + bank;
+    computerCoins.textContent = "Coins: " + bankCoins;
     playerCoins.textContent = "Coins: " + myCoins;
     checkCoins();
 }
